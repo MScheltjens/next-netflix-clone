a96f9884fa711a735c11d71282fafb498a996459
Mathias Scheltjens <mathias.scheltjens@icapps.com>
2023-02-15 11:53:24 +0100

    banner component setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto_Slab } from "@next/font/google";
+const inter = Roboto_Slab({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
