3541440eac7aa16b1fd68a3032925adb1760d6b6
Mathias Scheltjens <mathias.scheltjens@icapps.com>
2023-02-17 17:19:05 +0100

    magic added

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,17 +2,29 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
+import { MouseEvent, useState } from "react";
 
 import styles from "./login.module.css";
-import { EventHandler, MouseEvent, MouseEventHandler } from "react";
+import { useRouter } from "next/router";
 
-interface LoginProps {}
+const Login: NextPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const [userMsg, setUserMsg] = useState<string>("");
+  const router = useRouter();
 
-const Login: NextPage<LoginProps> = () => {
   const handleLoginEmail = (e: MouseEvent) => {
     e.preventDefault();
-    console.log("clicked");
+    if (email) {
+      if (email === "test@gmail.com") {
+        router.push("/");
+      } else {
+        setUserMsg("Email not found");
+      }
+    } else {
+      setUserMsg("Please enter your email");
+    }
   };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -40,8 +52,12 @@ const Login: NextPage<LoginProps> = () => {
             type="text"
             placeholder="Email address"
             className={styles.emailInput}
+            onChange={(e) => {
+              setUserMsg("");
+              setEmail(e.target.value);
+            }}
           />
-          <p className={styles.userMsg}></p>
+          <p className={styles.userMsg}>{userMsg}</p>
           <button onClick={handleLoginEmail} className={styles.loginBtn}>
             Sign In
           </button>
